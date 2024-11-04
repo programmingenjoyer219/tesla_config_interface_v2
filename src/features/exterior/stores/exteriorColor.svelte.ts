a7d67ac5213ff0d8294faa7dcbf9e2209577d61c
Yuganshu <yuganshu219@gmail.com>
2024-11-04 12:33:28 +0530

    shift to using 'object' data structure as state variable

--- src/features/exterior/stores/exteriorColor.svelte.ts
+++ src/features/exterior/stores/exteriorColor.svelte.ts
@@ -6,13 +6,6 @@ export type ExteriorColor =
 	| "stealth-grey"
 	| "ultra-red";
 
-let exteriorColor = $state<ExteriorColor>("deep-blue-metallic");
-
-export const exteriorColorStore = {
-	get value() {
-		return exteriorColor;
-	},
-	set value(v) {
-		exteriorColor = v;
-	},
-};
+export let exteriorColorStore = $state<{ value: ExteriorColor }>({
+	value: "deep-blue-metallic",
+});
--- src/features/interior/stores/interiorColor.svelte.ts
+++ src/features/interior/stores/interiorColor.svelte.ts
@@ -1,12 +1,5 @@
 export type InteriorColor = "light" | "dark";
 
-let interiorColor = $state<InteriorColor>("light");
-
-export const interiorColorStore = {
-	get value() {
-		return interiorColor;
-	},
-	set value(v) {
-		interiorColor = v;
-	},
-};
+export let interiorColorStore = $state<{ value: InteriorColor }>({
+	value: "light",
+});
